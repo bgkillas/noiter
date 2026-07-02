6257dc28ae176b9fe734084c677ca73d63cf0be2
bgkillas <walhelm@proton.me>
2026-07-01 23:48:52 -0400

    fix using width instead of height for min/max_y

--- src/load_chunks.rs
+++ src/load_chunks.rs
@@ -30,9 +30,9 @@ pub fn load_chunks(
     let px = (camera.translation.x / PIXEL_SCALE).floor() as u16;
     let py = (camera.translation.y / PIXEL_SCALE).floor() as u16;
     let min_x = px - world_image.width;
-    let min_y = py - world_image.width;
+    let min_y = py - world_image.height;
     let max_x = px + world_image.width;
-    let max_y = py + world_image.width;
+    let max_y = py + world_image.height;
     let min_cx = (min_x as usize / CHUNK_WIDTH).strict_cast::<ChunkIndexType>();
     let min_cy = (min_y as usize / CHUNK_HEIGHT).strict_cast::<ChunkIndexType>();
     let max_cx = (max_x as usize)
